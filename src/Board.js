0b6a07947349ebb58fc9c905dcd7492e9dcfa63f
Jezreel <jezreel.go@gmail.com>
2019-05-01 11:34:07 -0700

    add win check

--- src/Board.js
+++ src/Board.js
@@ -1,5 +1,25 @@
 import React, { useState } from 'react';
 
+const winnerCheck = (board) => {
+    const winningCombos = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+    let winner = ''
+    let winningArr = winningCombos.filter(c => {
+        return board[c[0]] !== '' && board[c[0]] === board[c[1]] && board[c[0]] === board[c[2]]
+    })
+    if(winningArr.length > 0){
+        winner = board[winningArr[0][0]]
+    }
+    return winner
+}
 
 const useBoard = () => {
     const [board, setBoard] = useState(['', '', '', '', '', '', '', '' ,''])
@@ -17,6 +37,10 @@ const useBoard = () => {
                 return player
             })
         }
+    }
+    let gameWin = winnerCheck(board)
+    if(gameWin){
+        alert(`${gameWin} is the winner!`)
     }
     return { board, attack }
 }
